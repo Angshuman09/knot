9aaf1ffb3212a13a7403d2f403fdfe13ed99be8c
Angshuman Kashyap <angshumankashyap69yap69@gmail.com>
2026-08-11 08:37:38 +0530

    add leader request handling

--- node/src/leader.rs
+++ node/src/leader.rs
@@ -1,6 +1,5 @@
 use std::sync::Arc;
 
-use common::ClientRequest::Withdraw;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::{Mutex, broadcast};
 
@@ -46,7 +45,7 @@ impl Leader{
                     Ok((socket, addr))=>{
                         let leader = client_leader.clone();
                         tokio::spawn(async move{
-                            if let Err(e) = leader.server_client(socket).await{
+                            if let Err(e) = leader.serve_client(socket).await{
                                 eprint!("client {addr} disconnected: {e}");
                             }
                         });
@@ -63,7 +62,7 @@ impl Leader{
                     Ok((socket, addr)) => {
                         let leader = follower_leader.clone();
                         tokio::spawn(async move{
-                            if let Err(e) = leader.server_client(socket).await{
+                            if let Err(e) = leader.serve_follower(socket).await{
                                 eprint!("follower {addr} disconnected: {e}");
                             }
                         });
@@ -77,7 +76,7 @@ impl Leader{
         Ok(())
     }
 
-    async fn server_client(&self, mut socket:TcpStream) -> std::io::Result<()>{
+    async fn serve_client(&self, mut socket:TcpStream) -> std::io::Result<()>{
         loop{
             let request: ClientRequest = wire::read_message(&mut socket).await?;
             let response = self.handle_request(request).await;
@@ -89,20 +88,75 @@ impl Leader{
     async fn handle_request(&self, request:ClientRequest) -> ClientResponse{
         match request{
             ClientRequest::Deposite { account, amount }=>{
-                println!("account: {account}, amount: {amount}");
-                ClientResponse::Balance { account, amount, as_of_offset: 0 }
+                self.commit(Event::Deposite { account, amount }).await
             }
             ClientRequest::Withdraw { account, amount }=>{
-                println!("account: {account}, amount: {amount}");
-                ClientResponse::Balance { account, amount, as_of_offset: 1 }
+                let mut ledger = self.ledger.lock().await;
+                if ledger.state.balance(&account) < amount{
+                    return ClientResponse::Error{
+                        message: format!("insufficient funds in {account}")
+                    };
+                }
+
+                let entry = Self::append_locked(&mut ledger, Event::Withdraw { account, amount });
+
+                drop(ledger);
+
+                let _ = self.new_entries.send(entry.clone());
+                ClientResponse::WriteAck { offset: entry.offset}
             }
             ClientRequest::Transfer { from, to, amount }=>{
-                println!("from: {from}, amount: {amount}");
-                ClientResponse::Balance { account: "abc".into(), amount, as_of_offset:  2}
+                let mut ledger = self.ledger.lock().await;
+                if ledger.state.balance(&from) < amount {
+                    return ClientResponse::Error{
+                        message: format!("insufficent balance in {from}")
+                    }
+                }
+
+                let transfer_id = ledger.log.last_offset() + 1;
+                let debit = Self::append_locked(
+                    &mut ledger,
+                     Event::TransferDebit { transfer_id, from, amount },
+                    );
+
+                    let credit = Self::append_locked(
+                        &mut ledger,
+                        Event::TransferCredit { transfer_id, to, amount }
+                    );
+
+                    drop(ledger);
+                    
+                    let _ = self.new_entries.send(debit);
+                    let _ = self.new_entries.send(credit.clone());
+                    ClientResponse::WriteAck { offset: credit.offset }
             }
-            ClientRequest::GetBalance { account, read_after }=>{
-                ClientResponse::Balance { account, amount: 0, as_of_offset: 3 }
+            ClientRequest::GetBalance { account, read_after: _ }=>{
+                let ledger = self.ledger.lock().await;
+                let amount = ledger.state.balance(&account);
+                ClientResponse::Balance { 
+                    account,
+                    amount,
+                    as_of_offset: ledger.log.last_offset() 
+                }
             }
         }
+    }
+
+    async fn commit(&self, event: Event) -> ClientResponse{
+        let mut ledger = self.ledger.lock().await;
+        let entry = Self::append_locked(&mut ledger, event);
+        drop(ledger);
+        let _ = self.new_entries.send(entry.clone());
+        ClientResponse::WriteAck { offset: entry.offset }
+    }
+
+    fn append_locked(ledger: &mut Ledger, event: Event) -> LogEntry{
+        let entry = ledger.log.append(event);
+        ledger.state.apply(&entry.event);
+        entry
+    }
+
+    async fn serve_follower(&self, mut socket: TcpStream) -> std::io::Result<()>{
+        Ok(())
     }
 }
